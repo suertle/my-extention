eee74756008417cfef5a13e96d22021fbf15ca6b
Suertle <teeraphat.btt@gmail.com>
2024-12-28 09:03:07 +0700

    adjust seeking time

--- remove-backdrop/scripts/oned.js
+++ remove-backdrop/scripts/oned.js
@@ -1,11 +1,7 @@
-console.log('start extention');
-
 setTimeout(() => {
     videoElement = document.querySelector('video[title="Advertisement"]');
 
     videoElement.addEventListener("loadedmetadata", () => {
-        videoElement.currentTime = videoElement.duration - 0.5;
+        videoElement.currentTime = videoElement.duration;
     });
 }, 3000);
-
-console.log(videoElement);
